f4482ebe145502ddc6f79148ffa5260680fa3c32
vivi-ana <acostaviviana.a@gmail.com>
2021-12-07 09:21:48 -0300

    Se agregan los test del modelo

---
+++ src/test/java/modelTests/AtraccionTests.java
@@ -0,0 +1,79 @@
+package modelTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Atraccion;
+import model.Producto;
+
+public class AtraccionTests {
+
+	Atraccion atraccion;
+
+	@Before
+	public void setUp() {
+		atraccion = new Atraccion(1, "Moria", 10, 2, 6, 1, 1);
+
+	}
+
+	@After
+	public void tearDown() {
+		atraccion = null;
+	}
+
+	@Test
+	public void creacionAtraccionTest() {
+		assertNotNull(atraccion);
+	}
+
+	@Test
+	public void restarCupoTest() {
+		atraccion.restarCupo();
+
+		int cupoObtenido = 5;
+		int cupoEsperado = atraccion.getCupo();
+
+		assertEquals(cupoObtenido, cupoEsperado);
+
+	}
+
+	@Test
+	public void hayCupoTest() {
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		assertTrue(atraccion.hayCupo());
+	}
+
+	@Test
+	public void noHayCupoTest() {
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		atraccion.restarCupo();
+		assertFalse(atraccion.hayCupo());
+
+	}
+
+	@Test
+	public void esPromocionTest() {
+		assertFalse(atraccion.esPromocion());
+	}
+
+	@Test
+	public void noEstaIncluidaTest() {
+		Producto atraccionIgual = new Atraccion(1, "Moria", 10, 2, 6, 1, 1);
+		Producto atraccionNoIgual = new Atraccion(2, "Isengard", 10, 2.5, 23, 1, 1);
+
+		assertTrue(atraccion.esOContiene(atraccionIgual));
+		assertFalse(atraccion.esOContiene(atraccionNoIgual));
+	}
+
+}
---
+++ src/test/java/modelTests/ItinerarioTests.java
@@ -0,0 +1,62 @@
+package modelTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Atraccion;
+import model.Itinerario;
+import model.Producto;
+import model.Usuario;
+
+public class ItinerarioTests {
+
+	Itinerario itinerario;
+	Usuario usuario;
+	Producto sugerencia1, sugerencia2, sugerencia3;
+	ArrayList<Producto> compras;
+
+	@Before
+	public void setup() {
+		usuario = new Usuario(1, "Sam", "pass", 1, 50, 3, 0, 1);
+		sugerencia1 = new Atraccion(1, "Edoras", 5, 0.5, 2, 1, 1);
+		sugerencia2 = new Atraccion(1, "Isengard", 5, 1, 2, 1, 1);
+		sugerencia3 = new Atraccion(3, "Rivendel", 10, 1, 2, 1, 1);
+		compras = new ArrayList<Producto>();
+	}
+
+	@After
+	public void tearDown() {
+		usuario = null;
+		sugerencia1 = null;
+		sugerencia2 = null;
+		sugerencia3 = null;
+		compras = null;
+	}
+
+	@Test
+	public void tiempoItinerario() {
+		// Verifica el gasto correcto de tiempo
+		compras.add(sugerencia1);
+		compras.add(sugerencia2);
+		compras.add(sugerencia3);
+		itinerario = new Itinerario(1, 1, compras);
+		assertEquals(2.5, itinerario.getDuracionItinerario(), 0);
+	}
+
+	@Test
+	public void monedasItinerario() {
+		// Verifica el gasto correcto de dinero.
+		compras.add(sugerencia1);
+		compras.add(sugerencia2);
+		compras.add(sugerencia3);
+		itinerario = new Itinerario(1, 1, compras);
+		assertEquals(20, itinerario.getCostoItinerario());
+	}
+
+
+}
