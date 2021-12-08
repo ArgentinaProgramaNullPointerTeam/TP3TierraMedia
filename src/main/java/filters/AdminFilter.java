79af7f743b3655c6a52177d490fce44dd9aa82b6
vivi-ana <acostaviviana.a@gmail.com>
2021-12-08 13:01:55 -0300

    Se agrega un filtro más

---
+++ src/main/java/filters/AdminFilter.java
@@ -0,0 +1,30 @@
+package filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import model.Usuario;
+
+@WebFilter(urlPatterns = "*.admin")
+public class AdminFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		Usuario usuario = (Usuario) ((HttpServletRequest) request).getSession().getAttribute("usuario");
+		if (usuario != null && usuario.isAdmin()) {
+			chain.doFilter(request, response);
+		} else {
+			request.setAttribute("flash", "No tenes los permisos necesarios");
+			RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/index.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+}
